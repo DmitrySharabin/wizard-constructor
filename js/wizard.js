db4417adc3dd955cfc58e7819e5ae60e40f900b6
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-03-29 12:44:28 +0300

    Добавил обработку пустого свойства only Теперь не нужно переживать за то, как оно будет обрабатываться ИРБ: его просто не будет в результирующем json-файле. Ура! Обожаю хуки Mavo!

--- js/wizard.js
+++ js/wizard.js
@@ -27,6 +27,17 @@
 						env.context.mavo.root.render(ret);
 					}
 				}
+			},
+
+			"node-getdata-end": env => {
+				if (env.context.property === "only") {
+					// Если свойство only имеет в качестве значения пустую строку,
+					// его не нужно сохранять. Для этого достаточно сообщить Mavo,
+					// что значение этого свойства null. Всё остальное Mavo сделает самостоятельно.
+					if (!env.data) {
+						env.data = null;
+					}
+				}
 			}
 		}
 	});
