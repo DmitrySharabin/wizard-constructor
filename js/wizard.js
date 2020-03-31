46f59302b179705fccd38220f99e2e7cea9f6a94
Dmitry Sharabin <dmitrysharabin@gmail.com>
2020-03-31 11:59:17 +0300

    Перенёс обработку КОИБов в более подходящее место В этом хуке данные имеют максимально простую структуру (просто объект) Манипулировать такими данными гораздо проще

--- js/wizard.js
+++ js/wizard.js
@@ -3,35 +3,31 @@
 (async () => {
 	Mavo.Plugins.register("wizard", {
 		hooks: {
-			"node-render-end": env => {
-				if (env.context.property === "only") {
-					// Поскольку мы не сохраняем значения промежуточных свойств,
-					// из которых собирается свойство only, при загрузке данных
-					// нужно выполнить обратную операцию, чтобы отразить в интерфейсе
-					// заданные при формировании пошаговой инструкции параметры об использовании КОИБ.
-					if (env.data) {
-						const koib = env.data["use_koib"];
-						let ret;
-
-						if (Array.isArray(koib)) {
-							ret = {
-								mode: "custom",
-								koib
-							};
-						} else {
-							ret = {
-								mode: koib
-							};
-						}
-
-						env.context.mavo.root.render(ret);
-					}
-				}
-			},
-
 			"render-start": env => {
 				if (env.context.id === "wizardConstructor") {
 					if (env.data) {
+						if (env.data.only) {
+							// Поскольку мы не сохраняем значения промежуточных свойств,
+							// из которых собирается свойство only, при загрузке данных
+							// нужно выполнить обратную операцию, чтобы отразить в интерфейсе
+							// заданные при формировании пошаговой инструкции параметры об использовании КОИБ.
+							const koib = env.data.only["use_koib"];
+							let ret;
+
+							if (Array.isArray(koib)) {
+								ret = {
+									mode: "custom",
+									koib
+								};
+							} else {
+								ret = {
+									mode: koib
+								};
+							}
+
+							$.extend(env.data, ret);
+						}
+
 						if (env.data.workflow) {
 							const data = env.data.workflow;
 
